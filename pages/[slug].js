0600e147f045353af035536e29a8241432ab52cf
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-03-24 11:28:52 +0000

    latest commit

---
+++ pages/[slug].js
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+
+export default function Post({ post }) {
+  return (
+    <div>
+      <Link href='/'>
+        <a>Go Home</a>
+      </Link>
+      <h2>{post.Title}</h2>
+      <p>{post.Content}</p>
+      <p>
+        <small>
+          {post.Slug} by {post.User.username}
+        </small>
+      </p>
+    </div>
+  );
+}
+
+// tell next.js how many pages there are
+export async function getStaticPaths() {
+  const res = await fetch('http://localhost:1337/posts');
+  const posts = await res.json();
+
+  const paths = posts.map(post => ({
+    params: { slug: post.Slug }
+  }));
+
+  return {
+    paths,
+    fallback: true
+  };
+}
+
+// get data for an individual page
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+
+  const res = await fetch(`http://localhost:1337/posts?Slug=${slug}`);
+  const data = await res.json();
+  const post = data[0];
+
+  return {
+    props: { post }
+  };
+}
